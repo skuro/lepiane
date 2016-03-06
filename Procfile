5a913ad7bbe8409b3bc812caac62846188c9cb3c
Carlo Sciolla <carlo.sciolla@gmail.com>
2016-03-06 10:41:43 +0100

    Project generated with `lein new reagent lepiane +test +devcards +cider`

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/lepiane.jar clojure.main -m lepiane.server
---
+++ test/cljs/lepiane/doo_runner.cljs
@@ -0,0 +1,5 @@
+(ns lepiane.doo-runner
+  (:require [doo.runner :refer-macros [doo-tests]]
+            [lepiane.core-test]))
+
+(doo-tests 'lepiane.core-test)
