405f6b2ad59717ad95108cb8fdcda69722e731da
Carlo Sciolla <carlo.sciolla@gmail.com>
2016-08-31 17:06:03 +0200

    adding missing files

---
+++ src/cljs/lepiane/data/services.cljs
@@ -0,0 +1,12 @@
+(ns lepiane.data.services)
+
+(def service-data
+  [{:pic "img/piscina.png"
+    :label "Piscina"
+    :link "#"}
+   {:pic "img/padronale.png"
+    :label "Auto"
+    :link "#"}
+   {:pic "img/padronale.png"
+    :label "Trasporto"
+    :link "#"}])
